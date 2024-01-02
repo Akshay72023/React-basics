c7167a0048355a9bf9541a3ba36c57bdcfd1df62
Akshay72023 <al7864745@gmail.com>
2024-01-02 21:14:43 +0530

    creating input form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,37 +1,37 @@
-import React,{ useState } from 'react';
+import React, { useState } from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [amount,setAmount]=useState(props.amount);
-  const clickHandler=()=>{
-    setAmount(100)
-    console.log(amount);
-    }
-  
-  // return (
-  //   <Card className='expense-item'>
-  //     <ExpenseDate date={props.date} />
-  //     <div className='expense-item__description'>
-  //       <h2>{title}</h2>
-  //       <div className='expense-item__price'>${props.amount}</div>
-  //       <button onClick={clickHandler}>Change title</button>
-  //     </div>
-  //   </Card>
-  // );
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  };
 
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${amount}</div>
-        <button onClick={clickHandler}>Change price</button>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>Change title</button>
       </div>
     </Card>
   );
-}
+
+  //   return (
+  //     <Card className='expense-item'>
+  //       <ExpenseDate date={props.date} />
+  //       <div className='expense-item__description'>
+  //         <h2>{props.title}</h2>
+  //         <div className='expense-item__price'>${amount}</div>
+  //         <button onClick={clickHandler}>Change price</button>
+  //       </div>
+  //     </Card>
+  //   );
+};
 
 export default ExpenseItem;
