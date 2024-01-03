bca6988b60c0e4a8adb17b64012fb5938db2c9cc
Akshay72023 <al7864745@gmail.com>
2024-01-03 14:07:15 +0530

    rendering new expenses with keys

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,19 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
-import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-
   return (
-    <Card className="expense-item">
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </Card>
   );
-
-  //   return (
-  //     <Card className='expense-item'>
-  //       <ExpenseDate date={props.date} />
-  //       <div className='expense-item__description'>
-  //         <h2>{props.title}</h2>
-  //         <div className='expense-item__price'>${amount}</div>
-  //         <button onClick={clickHandler}>Change price</button>
-  //       </div>
-  //     </Card>
-  //   );
-};
+}
 
 export default ExpenseItem;
